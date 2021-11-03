bd07e708ce6e2fc3d882aac5910d78b5c246ded1
Vitaliy Tsytsyk <s533720@nwmissouri.edu>
2021-11-03 17:24:45 -0500

    Milestone 5: Create /tv

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var tvRouter = require('./routes/tv');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/tv', tvRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
